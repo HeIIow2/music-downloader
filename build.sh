a2c06104447c49c0f1cb835ceb4aec78401dba18
Lars Noack <lars.noack@outlook.de>
2022-11-15 11:00:32 +0100

    updated build skript

--- build.sh
+++ build.sh
@@ -1,14 +1,20 @@
+echo "pushing............"
+git add .
+git commit -am "new build and upload"
+git push
+
+echo "compiling............"
 mkdir -p dist/build_files
 mkdir -p dist/compiled
 
 pyinstaller --onefile src/music_kraken_cli.py --specpath dist/build_files --workpath dist/build_files --distpath dist/compiled
 
 # https://packaging.python.org/en/latest/tutorials/packaging-projects/
-echo "building......"
+echo "building............"
 python3 -m pip install --upgrade build
 python3 -m build
 
-echo "uploading......."
+echo "uploading............"
 python3 -m pip install --upgrade twine
 twine upload dist/music_kraken*
 # twine upload --repository testpypi dist/*
