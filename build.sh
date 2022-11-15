a0ff7a0257813836aa596d501b3a0f3887ce5afd
Lars Noack <lars.noack@outlook.de>
2022-11-15 10:03:49 +0100

    dfas

--- build.sh
+++ build.sh
@@ -1,18 +1,13 @@
-# https://packaging.python.org/en/latest/tutorials/packaging-projects/
-# cd ..
-# cp ../README.md README.md
+pyinstaller --onefile src/run_cli.py
 
+exit
+
+# https://packaging.python.org/en/latest/tutorials/packaging-projects/
 echo "building......"
 python3 -m pip install --upgrade build
 python3 -m build
 
 echo "uploading......."
 python3 -m pip install --upgrade twine
-twine upload dist/*
+twine upload dist/music_kraken*
 # twine upload --repository testpypi dist/*
-
-# echo "cleaning up......."
-# rm README.md
-# cd ../build
-# rm -rf dist
-# mv ../src/dist .
---
+++ src/run_cli.py
@@ -0,0 +1,4 @@
+from music_kraken.__main__ import cli
+
+if __name__ == "__main__":
+    cli()
