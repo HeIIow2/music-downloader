19cd9c4e0b153725016e6468f52302afb96a3519
Hellow <Lars.Noack@outlook.de>
2022-11-24 22:10:22 +0100

    modified garbage collection to run a bit less often

--- src/music_kraken/__init__.py
+++ src/music_kraken/__init__.py
@@ -1,3 +1,5 @@
+import gc
+
 from typing import List
 import musicbrainzngs
 import logging
@@ -16,6 +18,20 @@ from .utils.shared import (
 )
 
 from .lyrics import lyrics
+
+"""
+At the start I modify the garbage collector to run a bit fewer times.
+This should increase speed:
+https://mkennedy.codes/posts/python-gc-settings-change-this-and-make-your-app-go-20pc-faster/
+"""
+# Clean up what might be garbage so far.
+gc.collect(2)
+
+allocs, gen1, gen2 = gc.get_threshold()
+allocs = 50_000  # Start the GC sequence every 50K not 700 allocations.
+gen1 = gen1 * 2
+gen2 = gen2 * 2
+gc.set_threshold(allocs, gen1, gen2)
 
 logging.getLogger("musicbrainzngs").setLevel(logging.WARNING)
 musicbrainzngs.set_useragent("metadata receiver", "0.1", "https://github.com/HeIIow2/music-downloader")
