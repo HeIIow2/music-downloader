ded2f7b529e2f3e8d138a2516ae7d0637fba3850
Lars Noack <lars.noack@outlook.de>
2022-12-02 10:59:22 +0100

    started new Databse Schematic

--- src/music_kraken/static_files/new_db.sql
+++ src/music_kraken/static_files/new_db.sql
@@ -12,20 +12,6 @@ CREATE TABLE Source
     FOREIGN KEY(song_id) REFERENCES Song(id)
 );
 
-CREATE TABLE Target
-(
-    id      BIGINT AUTO_INCREMENT PRIMARY KEY,
-    song_id BIGINT,
-    FOREIGN KEY(song_id) REFERENCES Song(id)
-);
-
-CREATE TABLE Lyrics
-(
-    id      BIGINT AUTO_INCREMENT PRIMARY KEY,
-    song_id BIGINT,
-    FOREIGN KEY(song_id) REFERENCES Song(id)
-);
-
 CREATE TABLE Artist
 (
     id      INTEGER AUTO_INCREMENT PRIMARY KEY, 
@@ -40,6 +26,23 @@ CREATE TABLE Album
     FOREIGN KEY(song_id) REFERENCES Song(id)
 );
 
+CREATE TABLE Target
+(
+    id      BIGINT AUTO_INCREMENT PRIMARY KEY,
+    song_id BIGINT,
+    FOREIGN KEY(song_id) REFERENCES Song(id)
+);
+
+CREATE TABLE Lyrics
+(
+    id      BIGINT AUTO_INCREMENT PRIMARY KEY,
+    song_id BIGINT,
+    FOREIGN KEY(song_id) REFERENCES Song(id)
+);
+
+CREATE TABLE EasyId3
+(
+);
 
 CREATE TABLE SongArtist
 (
@@ -56,3 +59,9 @@ CREATE TABLE AlbumArtist
       FOREIGN KEY(album_id) REFERENCES Album(id),
       FOREIGN KEY(artist_id) REFERENCES Artist(id)
 );
+
+
+SELECT 
+    Song.id,
+    Song.name
+FROM Song
