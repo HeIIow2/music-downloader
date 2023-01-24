c1dbb62eed9482f73c915296a8b4390b65064f84
Lars Noack <lars.noack@outlook.de>
2023-01-24 14:37:37 +0100

    dasf

--- src/goof.py
+++ src/goof.py
@@ -2,7 +2,7 @@ from music_kraken.pages import (
     EncyclopaediaMetallum
 )
 
-print(EncyclopaediaMetallum.search_by_query("Psychonaut"))
+print(EncyclopaediaMetallum.search_by_query("only smile"))
 print(EncyclopaediaMetallum.search_by_query("#a Ghost Bath"))
 print(EncyclopaediaMetallum.search_by_query("#a Ghost Bath #r Self Loather"))
 print(EncyclopaediaMetallum.search_by_query("#a Ghost Bath #r Self Loather #t hide from the sun"))
