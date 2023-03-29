f4047b91c1a6016b1bf3701e180e4cb2387edb06
Hellow2 <lars.noack@outlook.de>
2023-03-29 15:22:18 +0200

    defined shorthands

--- src/music_kraken/pages/download_center/page_attributes.py
+++ src/music_kraken/pages/download_center/page_attributes.py
@@ -1,8 +1,12 @@
-from typing import Tuple, Type
+from typing import Tuple, Type, Dict
 
 from ..abstract import Page
 from ..encyclopaedia_metallum import EncyclopaediaMetallum
 from ..musify import Musify
+
+page_names: Dict[str, Type[Page]] = dict()
+
+shorthand_of_page: Dict[Type[Page], str] = dict()
 
 ALL_PAGES: Tuple[Type[Page]] = (
     EncyclopaediaMetallum,
@@ -17,3 +21,10 @@ SHADY_PAGES: Tuple[Type[Page]] = (
     Musify,
 )
 
+
+SHORTHANDS = ('a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z')
+for i, page in enumerate(ALL_PAGES):
+    page_names[page.__name__] = page
+    page_names[SHORTHANDS[i]] = page
+    
+    shorthand_of_page[page] = SHORTHANDS[i]
