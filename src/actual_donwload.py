b40174fb97d7ba5915ce11a516c036cea89649a0
Hellow <Lars.Noack@outlook.de>
2023-03-29 20:56:57 +0200

    Update actual_donwload.py

--- src/actual_donwload.py
+++ src/actual_donwload.py
@@ -3,7 +3,7 @@ from music_kraken import pages
 
 def cli():
     search = pages.Search()
-    
+
     while True:
         search.next_input(input(">> "))
         print(search)
@@ -13,19 +13,17 @@ def search_pages():
     search = pages.Search()
     print("metadata", search.pages)
     print("audio", search.audio_pages)
-    
+
     print()
     print(search)
-    
+
     search.choose(pages.Musify)
-    
+
     print()
     print(search)
-    
+
     search.choose(0)
     print(search)
-
-
 
 
 if __name__ == "__main__":
