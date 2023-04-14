8b0beeba90c12bbbf975d3b4fa8ea05380d21e14
Ahmet <63475369+ahmetlii@users.noreply.github.com>
2023-04-14 19:01:18 +0300

    init

--- src/music_kraken/pages/youtube.py
+++ src/music_kraken/pages/youtube.py
@@ -22,17 +22,20 @@ from ..utils import (
     string_processing
 )
 
-INVIDIOUS_INSTANCE = "https://yewtu.be/feed/popular"
+INVIDIOUS_INSTANCE = "https://yt.artemislena.eu/api"
+"""
+NOTE: Implement a switch for different instances, 
+Current one is a wacky solution and needs to be changed for
+all usecases.
+"""
 
 class Youtube(Page):
     """
-    The youtube downloader should use https://invidious.io/
-    to make the request.
-    They are an alternative frontend.
+    This is an abstract class for YouTube downloader.
 
     To find an artist filter for chanel and search for
     `{artist.name} - Topic`
-    and then ofc check for viable results.
+    and then checks for viable results.
 
     Ofc you can also implement searching songs by isrc.
 
@@ -42,5 +45,72 @@ class Youtube(Page):
     I don't wanna maintain that shit. 
     """
     API_SESSION: requests.Session = requests.Session()
+    API_SESSION.proxies = shared.proxies
+    TIMEOUT = 5
+    POST_TIMEOUT = TIMEOUT
+    TRIES = 5
+    LOGGER = logging.getLogger("youtube")
 
-    SOURCE_TYPE = SourcePages.YOUTUBE
+    @classmethod
+    def search_by_query(cls, query: str) -> List[MusicObject]:
+        query_obj = cls.Query(query)
+
+        #if query_obj.is_raw:
+        return cls.simple_search(query_obj)
+        #return cls.advanced_search(query_obj)
+
+    @classmethod
+    def simple_search(cls, query: Page.Query) -> List[Artist]:
+        """
+        Searches the default endpoint as is.
+
+        NOTE: Nor YouTube neither Invidious doesn't seem to provide 
+        a meaningful way of systematically parsing artist, song
+        and all other fields, I rather trust in YouTube's search
+        to match it well and pass channel names as artist. Note 
+        that some artists don't have accounts on YouTube but 
+        rather licensed the music.
+        TODO: implement playlist searching or something similar to Spotube as a workaround?
+        """
+        endpoint = "https://yt.artemislena.eu/api/v1/search/?q={query}"
+
+        r = cls.API_SESSION.get(endpoint.format(query=query))
+        if r.status_code != 200:
+            LOGGER.warning(f"code {r.status_code} at {endpoint.format(query=query.query)}")
+            return []
+
+        return [
+            cls(title=getdata[1], url=getdata[2], artist=getdata[3])
+            for getdata in r.json()['']
+        ]
+    
+    @classmethod
+    def _fetch_song_from_source(cls, source: Source, stop_at_level: int = 1) -> Song:
+        return Song(
+            title=cls(title)
+        )
+
+    @classmethod
+    def _fetch_album_from_source(cls, source: Source, stop_at_level: int = 1) -> Album:
+        #return Album()
+        return None
+    @classmethod
+    def _fetch_artist_from_source(cls, source: Source, stop_at_level: int = 1) -> Artist:
+        return Artist(
+            artist=cls(artist)
+        )
+
+    @classmethod
+    def _fetch_label_from_source(cls, source: Source, stop_at_level: int = 1) -> Label:
+        #return Label()
+        return None
+
+    @classmethod
+    def _get_type_of_url(cls, url: str) -> Optional[Union[Type[Song], Type[Album], Type[Artist], Type[Label]]]:
+        return None
+
+    @classmethod
+    def _download_song_to_targets(cls, source: Source, target: Target, desc: str = None) -> DownloadResult:
+        return DownloadResult(
+            url=cls(url)
+        )
---
+++ src/yt_fetch.py
@@ -0,0 +1,14 @@
+from music_kraken import (
+    Song,
+    Database,
+    Artist,
+    Album
+)
+
+from music_kraken.pages import (
+    YouTube
+)
+
+
+test_db = Database("test.db")
+# test_db.reset()
